76b89919fc84cb0d0115e7a5e3e29ff32dc69fbc
Hubert Fratczak <hubert.fratczak@onet.com.pl>
2025-10-16 10:58:49 +0200

    fetch pokemon data

--- memory-card-pokemon/src/App.jsx
+++ memory-card-pokemon/src/App.jsx
@@ -1,33 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
+import { PokemonCard } from './components/PokemonCard'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+function App() {
+  const pokemonNames = ["rattata", "pidgeotto", "beedrill", "weedle", "metapod", "blastoise"]
+  return (<>
+    <header>
+      <div className="game-title">Pokemon Memory Card</div>
+      <div className="score-container">
+        <div className="score">Score: </div>
+        <div className="best-score">Best Score:</div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    </header>
+    <div className="pokemon-section">
+      {pokemonNames.map(name => (
+        <PokemonCard key={name} name={name}></PokemonCard>
+      )
+      )}
+    </div>
     </>
   )
 }
---
+++ memory-card-pokemon/src/components/PokemonCard.jsx
@@ -0,0 +1,34 @@
+import { useState, useEffect } from "react"
+import "./PokemonCard.css"
+export function PokemonCard({name}) {
+    const [pokemonData, setPokemonData] = useState(null);
+    useEffect(() => {
+        const fetchPokemonData = async () => {
+            try {
+                const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
+                const data = await response.json()
+                setPokemonData(data)
+                console.log(data)
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        fetchPokemonData()
+    }, [name])
+
+    return (
+        <div className="pokemon-container">{!pokemonData ? ("Loading...")
+            : (<>
+                <div className="image-container">
+                    <img
+                        src={pokemonData.sprites.other.dream_world.front_default}
+                        alt={pokemonData.name}
+                        className="pokemon-image">
+                    </img>
+                </div>
+                <div className="pokemon-name">{pokemonData.name}</div>
+            </>
+            )
+        }</div>
+    )
+}
